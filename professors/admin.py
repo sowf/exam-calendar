e5e3090315ae03236442f709c3e2dc390c06bdbd
sowf <mischenkovofficial@gmail.com>
2021-01-17 13:17:07 +0300

    Added new models in professors' app

--- professors/admin.py
+++ professors/admin.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
-from .models import University, Professor, ProfessorRate
+from .models import University, Subject, Professor, ProfessorRate, ProfessorStory, SubjectRate
 
 
 class UniversityAdmin(admin.ModelAdmin):
     list_display = ('name', 'slug')
 
 admin.site.register(University, UniversityAdmin)
+admin.site.register(Subject)
 admin.site.register(Professor)
 admin.site.register(ProfessorRate)
+admin.site.register(ProfessorStory)
+admin.site.register(SubjectRate)
--- professors/models.py
+++ professors/models.py
@@ -14,11 +14,21 @@ class University(models.Model):
         verbose_name = 'Университет'
         verbose_name_plural = 'Университеты'
 
+class Subject(models.Model):
+    name = models.CharField('Название',max_length=150)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Предмет'
+        verbose_name_plural = 'Предметы'
+
 class Professor(models.Model):
     full_name = models.CharField('ФИО', max_length=150)
     birth_date = models.DateField('Дата рождения')
     is_checked = models.BooleanField('Проверен?', default=False)
-    # subject = models.ForeignKey()
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
     university = models.ForeignKey(University, on_delete=models.CASCADE)
 
     def __str__(self):
@@ -46,3 +56,30 @@ class ProfessorRate(models.Model):
     class Meta:
         verbose_name = 'Рейтинг профессора'
         verbose_name_plural = 'Рейтинги профессоров'
+
+class ProfessorStory(models.Model):
+    professor = models.ForeignKey(Professor, on_delete=models.CASCADE)
+    text = models.TextField('Байка о профессоре')
+
+    class Meta:
+        verbose_name = 'История о профессоре'
+        verbose_name_plural = 'Истории о профессорах'
+
+class SubjectRate(models.Model):
+    rate = models.PositiveIntegerField(
+        'Рейтинг',
+        default=0,
+        validators=[
+            MaxValueValidator(100),
+            MinValueValidator(0)
+        ]
+    )
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.rate)
+
+    class Meta:
+        verbose_name = 'Рейтинг предмета'
+        verbose_name_plural = 'Рейтинги предметов'
